a84d2925eb6f47bb44fe8204051f7a3340b6ddb7
Dlopez079 <dlopez@lopezdavid.dev>
2020-05-03 13:59:41 -0400

    Removed bg-white from the navbar on app so I can control the navbar color from the scss file.  I can see the navbar color changing as I make the change on code.

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -6,7 +6,7 @@
                     <div class="card-header">Example Component</div>
 
                     <div class="card-body">
-                        I'm an example component - EDITED.
+                        I'm an example component - EDITED by David.
                     </div>
                 </div>
             </div>
