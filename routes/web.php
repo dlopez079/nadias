19aafc30733dc9516e20e47c325970a5a0f25b4b
Dlopez079 <dlopez@lopezdavid.dev>
2020-05-04 12:50:40 -0400

    Modified '/home' to '/' on all controllers.

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes(['verify' => true]);
 
-Route::get('/', 'HomeController@index')->name('home')
+Route::get('/', 'HomeController@index')->name('home');
